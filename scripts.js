ebf9dec3a38bd5a50b8b57055905036d0952bb66
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-06 16:01:23 +0200

    turn generateGrid into a function.

--- scripts.js
+++ scripts.js
@@ -3,19 +3,21 @@ const container = document.querySelector(".container");
 
 let size = 16;
 
-//We'll have rows now. 
-for (let i = 0; i < size; i++) {
-  //This creates a row within the big container.
-  let row = document.createElement("div");
-  row.classList.toggle("row");
-  container.appendChild(row);
-  for (let j = 0; j < size; j++) {
-    //Creates a pixel within a row.
-    let pixel = document.createElement("div");
-    pixel.classList.toggle("pixel");
-    row.appendChild(pixel);
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = "grey";
-    })
+
+function generateGrid(size) {
+  for (let i = 0; i < size; i++) {
+    //This creates a row within the big container.
+    let row = document.createElement("div");
+    row.classList.toggle("row");
+    container.appendChild(row);
+    for (let j = 0; j < size; j++) {
+      //Creates a pixel within a row.
+      let pixel = document.createElement("div");
+      pixel.classList.toggle("pixel");
+      row.appendChild(pixel);
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "grey";
+      })
+    }
   }
 }

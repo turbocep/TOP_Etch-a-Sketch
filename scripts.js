986835214aceba9b13173711b093201804c24439
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-06 15:48:49 +0200

    generate rows instead of direct divs.
    I tried to make the drawing surface divs (pixels)
    resize to their container using flexbox. The
    problem was that they'd only resize into their
    flex direction, but not on the cross-axis. This
    caused the divs to not spread along the cross
    direction (vertically) and they'd all bunch-up
    horizontally, leaving empty space at the bottom
    of the container. I'm solving this by creating
    extra div containers called "rows" that the
    pixels sit in. Now flexbox can be used to size
    the pixels in the horizontal directions, and the
    rows will size the pixels in the vertical
    direction.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 //Reference selectors
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  let div = document.createElement("div");
-  div.classList.toggle("div");
-  container.appendChild(div);
+let size = 16;
+
+//We'll have rows now. 
+for (let i = 0; i < size; i++) {
+  //This creates a row within the big container.
+  let row = document.createElement("div");
+  row.classList.toggle("row");
+  container.appendChild(row);
 }
